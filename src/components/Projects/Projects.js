eb24561a77704a68ac44a4b066d3bab0c25102b7
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-07-22 12:55:27 +0530

    added figma project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,7 @@ import float from "../../Assets/Projects/float.png";
 import flex from "../../Assets/Projects/flex.png";
 import blog from "../../Assets/Projects/blog.png";
 import Contactlist from "../../Assets/Projects/contactlist.png"
+import figdissc from "../../Assets/Projects/figdissc.png"
 
 function Projects() {
   return (
@@ -21,6 +22,15 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={figdissc}
+              isBlog={false}
+              title="MediaMate project design in figma"
+              description="The Figma design for MediaMate focuses on a clean and user-friendly interface with a modern aesthetic. The design adheres to principles of simplicity, consistency, and accessibility to ensure a seamless user experience across various devices."
+              demoLink="https://www.figma.com/proto/PA3Ca41eXI77MupX9HCEiA/Social-media-design?t=bg6fLkwRuwWDtKqO-1"
+            />
+          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
@@ -31,7 +41,7 @@ function Projects() {
                providing users with the ability to add, edit, and manage their contacts efficiently. ContactList streamlines the process of contact management,
                 making it an essential tool for organizing and staying connected."
               ghLink="https://github.com/thasleemkhan762/react-redux-contactlist.git"
-              demoLink="https://react-redux-contactlist.vercel.app/"      
+              demoLink="https://react-redux-contactlist.vercel.app/"
             />
           </Col>
 
